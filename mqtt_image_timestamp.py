583f9d78d1b526f54148ee626741ffea0a45db21
jerryneedell <jerryneedell@gmail.com>
2026-01-12 18:28:01 -0500

    add timestamping

--- mqtt_image_timestamp.py
+++ mqtt_image_timestamp.py
@@ -6,16 +6,11 @@
 import sys
 import binascii
 from PIL import Image
-import os
-from datetime import datetime
 # Import Adafruit IO MQTT client.
 from Adafruit_IO import MQTTClient
+import os
+from datetime import datetime
 
-# Import smtplib for the actual sending function.
-import smtplib
-
-# Here are the email package modules we'll need.
-from email.message import EmailMessage
 
 aio_username = os.environ.get("ADAFRUIT_IO_USERNAME")
 aio_key = os.environ.get("ADAFRUIT_IO_KEY")
@@ -47,45 +42,19 @@ def message(client, feed_id, payload):
     # Message function will be called when a subscribed feed has a new value.
     # The feed_id parameter identifies the feed, and the payload parameter has
     # the new value.
-#    print('Feed {0} received new value: {1}'.format(feed_id, payload))
-    print('Feed {0} received new image'.format(feed_id))
+    print('Feed {0} received new value: {1}'.format(feed_id, payload))
     jpeg_image = binascii.a2b_base64(payload)
     base_filename = 'esp32s3revtft.jpeg'
     timestamp = datetime.now().strftime('%Y%m%d%H%M')
     new_filename = f"{base_filename.split('.')[0]}_{timestamp}.{base_filename.split('.')[-1]}"
     with open(new_filename, "wb") as file:
         file.write(jpeg_image)
-#    new_image=Image.open('test.jpeg')
-#    new_image.show()
-    # Create the container email message.
-    msg = EmailMessage()
-    msg['Subject'] = 'image test'
-    icloud_username = os.environ.get("ICLOUD_EMAIL")
-    recipient = os.environ.get("IMAGE_RECIPIENT")
-    msg['From'] = icloud_username
-    msg['To'] = recipient
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-
-    msg.add_attachment(jpeg_image, maintype='image',
-                             subtype='jpeg')
 
     try:
-        # Connect to the iCloud SMTP server and start a secure session
-        with smtplib.SMTP('smtp.mail.me.com', 587) as server:
-            server.starttls()
-            # Log in with your Apple ID and app-specific password
-            username = os.environ.get("AIOIMAGE_USERNAME")
-            password = os.environ.get("AIOIMAGE_PASSWORD")
-            server.login(username, password)
-            # Send the message
-            server.send_message(msg)
-            print("Email sent successfully!")
-
-    except smtplib.SMTPException as e:
-        print(f"Error: Unable to send email. {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
+        new_image=Image.open(new_filename)
+        new_image.show()
+    except IOError as e:
+        print("Error: Image file truncated or corrupted")
 
 # Create an MQTT client instance.
 client = MQTTClient(aio_username, aio_key)
@@ -103,6 +72,3 @@ client.connect()
 # received.  Note there are other options for running the event loop like doing
 # so in a background thread--see the mqtt_client.py example to learn more.
 client.loop_blocking()
-
-
-
